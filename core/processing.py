297f6d719820cfd589159f16584b24a8016e8c58
casper <johnwaiton@hotmail.co.uk>
2025-12-18 12:48:32 +0000

    add data path, alter directory logic

--- core/processing.py
+++ core/processing.py
@@ -10,7 +10,7 @@ from pathlib    import Path
 from omegaconf  import DictConfig
 
 from core.io        import print_cfg
-from core.config_io import generate_configs
+#from core.config_io import generate_configs
 from core.config_io import alter_config
 
 from core.immutables import processing_style
@@ -26,10 +26,16 @@ def process_IC(global_vars : Dict,
 
     # create directory for storing configs
     config_name = f"{name}-{global_vars['tag']}-{global_vars['timestamp']}"
-    # config path based on city and run number
+
+    # create config location
     specific_config_path = os.path.join(global_vars['config_path'], cfg['city'])
     specific_config_path = os.path.join(specific_config_path, cfg['run_number'])
     Path(specific_config_path).mkdir(parents = True, exist_ok = True)
+
+    # create data location
+    specific_data_path = os.path.join(global_vars['data_path'], cfg['city'])
+    specific_data_path = os.path.join(specific_data_path, cfg['run_number'])
+    Path(specific_data_path).mkdir(parents = True, exist_ok = True)
 
     proc_style = processing_style[global_vars['processing_style']]
 
@@ -41,17 +47,20 @@ def process_IC(global_vars : Dict,
     config = config_path.read_text()
     config = alter_config(config, cfg)
 
-    match proc_style:
-        case processing_style.LDC:
+    match global_vars['processing_style']:
+        case "LDC":
             # generate folders
             for i in range(global_vars['LDCs']):
-                Path(f'{specific_config_path}/LDC{i}').mkdir(parents = True, exist_ok = True)
-                logging.info(f'Generated config location at {specific_config_path}/LDC{i}')
-
+                Path(f'{specific_config_path}/LDC{i+1}').mkdir(parents = True, exist_ok = True)
+                Path(f'{specific_data_path}/LDC{i+1}').mkdir(parents = True, exist_ok = True)
+                logging.info(f'Generated config location at {specific_config_path}/LDC{i+1}')
                 # generate all config files based on input files
-        case processing_style.FOLDER:
+
+
+
+        case "FOLDER":
             print('folder')
-        case processing_style.FILE:
+        case "FILE":
             print('file')
         case _:
             print('something else (fuck up)')
@@ -59,11 +68,11 @@ def process_IC(global_vars : Dict,
 
 
     # extract the base config template
-    generate_configs(global_vars,
-                     cfg,
-                     config_name,
-                     proc_style)
-    # create jobs to execute these configs
+    #generate_configs(global_vars,
+    #                 cfg,
+    #                 config_name,
+    #                 proc_style)
+    ## create jobs to execute these configs
 
 
     # run the jobs
