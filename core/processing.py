4f9f301c3c54e9feee1ce19850f77d7d8c3a459a
casper <johnwaiton@hotmail.co.uk>
2025-12-18 17:22:54 +0000

    simplify processing by removing sweeps and functionalisation

--- core/processing.py
+++ core/processing.py
@@ -11,7 +11,7 @@ from omegaconf  import DictConfig
 
 from core.io        import print_cfg
 #from core.config_io import generate_configs
-from core.config_io import alter_config, generate_folder_structure
+from core.config_io import alter_config, generate_folder_structure, collect_input_names, extract_output_names, write_configs
 
 from core.immutables import processing_style
 
@@ -27,72 +27,23 @@ def process_IC(global_vars : Dict,
     # create directory for storing configs
     config_name = f"{name}-{global_vars['tag']}-{global_vars['timestamp']}"
 
+    # generate_folder_structure
+    output_config_path, output_data_path = generate_folder_structure(global_vars, cfg)
+
     # define the generalised config shape here
     config_path = Path(f"{PROD_DIR}/configs/IC_configs/{cfg['city']}.conf")
     logging.info(f'Read/alter config from {config_path}')
     config = config_path.read_text()
+    # intially alter to match fixed components
     config = alter_config(config, cfg)
 
-    # generate folder structure
-    generate_folder_structure(global_vars, cfg)
-    # extract the specific sweep for this run
-    '''
-    parameter_folder = ''
-    if cfg['sweep_params']:
-        for param in cfg['sweep_params']:
-            parameter_folder += param
-            parameter_folder += str(cfg[param])
-            parameter_folder += '_'
-        # remove trailing _
-        parameter_folder = parameter_folder[:-1]
+    # collect all input file names
+    input_names = collect_input_names(global_vars, cfg)
+    output_names, config_names = extract_output_names(global_vars, cfg, input_names)
 
-    # create config location
-    specific_config_path = os.path.join(global_vars['config_path'], cfg['city'])
-    specific_config_path = os.path.join(specific_config_path, cfg['run_number'])
-    if parameter_folder != '':
-        specific_config_path = os.path.join(specific_config_path, parameter_folder)
-
-    Path(specific_config_path).mkdir(parents = True, exist_ok = True)
-
-    # create data location
-    specific_data_path = os.path.join(global_vars['data_path'], cfg['city'])
-    specific_data_path = os.path.join(specific_data_path, cfg['run_number'])
-    if parameter_folder != '':
-        specific_data_path = os.path.join(specific_data_path, parameter_folder)
-
-    Path(specific_data_path).mkdir(parents = True, exist_ok = True)
-
-    proc_style = processing_style[global_vars['processing_style']]
-
-    logging.info(f'Generating configs with proc_style: {proc_style}')
-
-
-    match global_vars['processing_style']:
-        case "LDC":
-            # generate folders
-            for i in range(global_vars['LDCs']):
-                Path(f'{specific_config_path}/ldc{i+1}').mkdir(parents = True, exist_ok = True)
-                Path(f'{specific_data_path}/ldc{i+1}').mkdir(parents = True, exist_ok = True)
-                logging.info(f'Generated config location at {specific_config_path}/ldc{i+1}')
-                # generate all config files based on input files
-
-
-
-        case "FOLDER":
-            print('folder')
-        case "FILE":
-            print('file')
-        case _:
-            print('something else (fuck up)')
-
-
-    '''
-    # extract the base config template
-    #generate_configs(global_vars,
-    #                 cfg,
-    #                 config_name,
-    #                 proc_style)
-    ## create jobs to execute these configs
-
+    write_configs(input_names, output_names, config_names, config, global_vars)
 
     # run the jobs
+
+def process_binary(global_vars, config_dict, name):
+    print('binary')
