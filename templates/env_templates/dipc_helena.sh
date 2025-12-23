f2433aa73ad3feaa7ca3f465b8d47c80d114406c
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2025-12-23 15:14:46 +0000

    include DIPC template

---
+++ templates/env_templates/dipc_helena.sh
@@ -0,0 +1,23 @@
+
+echo "Initialise conda"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/halmazan/miniconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/halmazan/miniconda/etc/profile.d/conda.sh" ]; then
+        . "/home/halmazan/miniconda/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/halmazan/miniconda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+#
+echo "Config produced, initialising IC"
+# Initialise IC
+conda init bash
+source /scratch/halmazan/NEXT/IC_alter-blob-centre/init_IC.sh
+conda activate IC-3.8-2024-06-08
