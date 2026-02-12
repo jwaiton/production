3f8b0b9c78aa891ad65a5ea13c9ec209d32b38a0
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2026-02-12 13:26:44 +0000

    include bespoke noether environment template

---
+++ templates/env_templates/noether_icjohn.sh
@@ -0,0 +1,6 @@
+source /gluster/data/next/software/miniconda/etc/profile.d/conda.sh
+export ICTDIR=/gluster/data/next/software/IC_john/IC
+export ICDIR=$ICTDIR/invisible_cities
+export PATH="$ICTDIR/bin:$PATH"
+export PYTHONPATH=$ICTDIR:$PYTHONPATH
+conda activate IC-3.8-2024-06-08 
